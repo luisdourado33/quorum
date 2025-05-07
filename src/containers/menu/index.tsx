d6bf922339d976684a5049284c7a60e3fbee735a
Luís Dourado <luis_dourado33@hotmail.com>
2025-05-06 23:12:18 -0400

    feat(menu): add Menu component for navigation and theme switching

---
+++ src/containers/menu/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import MainNavigation from "../main-navigation";
+import Brand from "@/components/brand";
+
+import { twMerge } from "tailwind-merge";
+import ThemeSwitcher from "@/components/theme-switcher";
+
+export default function Menu() {
+  return (
+    <div
+      className={twMerge([
+        "sticky top-0 left-0 z-50",
+        "flex w-full items-center justify-center",
+        "shadow-sm",
+        "backdrop-blur-2xl",
+        "py-4",
+        "mb-4",
+      ])}
+    >
+      <div className="container flex items-center justify-between">
+        <Brand />
+        <MainNavigation className="hidden md:flex" />
+        <ThemeSwitcher />
+      </div>
+    </div>
+  );
+}
