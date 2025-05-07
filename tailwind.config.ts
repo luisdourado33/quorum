01b04161197990f4242445e79548c991b5dbb5ec
Luís Dourado <luis_dourado33@hotmail.com>
2025-05-07 08:58:04 -0400

    feat(tailwind): enhance container configuration with custom padding and screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,29 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    corePlugins: {
+      container: false,
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+        screens: {
+          sm: "300px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+          "2xl": "1496px",
+        },
       },
     },
   },
