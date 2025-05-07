4317c0c8b967fa814d32ff4ea989af532d539bbe
Luís Dourado <luis_dourado33@hotmail.com>
2025-05-07 08:58:10 -0400

    fix(tailwind): disable core container plugin in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,6 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    corePlugins: {
-      container: false,
-    },
     extend: {
       colors: {
         background: "var(--background)",
