128e3985b91e8a4dae04c1c450bb14de54f8eff7
Luís Dourado <luis_dourado33@hotmail.com>
2025-05-06 23:58:15 -0400

    feat(legislators-table): refactor ServerLegislatorsTable to use simulateServerRequest for data fetching

--- src/containers/legislators-table/index.tsx
+++ src/containers/legislators-table/index.tsx
@@ -6,12 +6,13 @@ import RenderLegislatorsTable from "./render-table";
 
 import { getLegislatorsWithBillStats } from "@/actions/bill";
 import { type ILegislatorTableColumns } from "@/types/legislator";
+import { simulateServerRequest } from "@/lib/utils";
+import { DELAY_REQUEST_TIMEOUT } from "@/utils/delay";
 
 export default async function ServerLegislatorsTable() {
-  const legislators: ILegislatorTableColumns[] = await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(getLegislatorsWithBillStats());
-    }, 1000)
+  const legislators: ILegislatorTableColumns[] = await simulateServerRequest(
+    getLegislatorsWithBillStats(),
+    DELAY_REQUEST_TIMEOUT
   );
 
   return <RenderLegislatorsTable data={legislators} />;
